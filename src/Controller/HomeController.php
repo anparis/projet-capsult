1b436649dcb45d5fb660edcbda1a84cfa678bfff
Antonin Paris <antonin.pariss@gmail.com>
2023-03-23 17:57:50 +0100

    Add User auth

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,30 +2,15 @@
 
 namespace App\Controller;
 
-use DateTime;
-use App\Entity\Bloc;
-use App\Entity\Lien;
-use App\Entity\Image;
-use App\Service\Validation;
-use App\Service\FileUploader;
-use App\Repository\BlocRepository;
-use App\Repository\LienRepository;
-use App\Repository\ImageRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
   #[Route('/', name: 'app_home')]
-  public function home(BlocRepository $bl): Response
+  public function home(): Response
   {
     return new Response('HOME');
   }
-
-  
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -9,13 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/{slug}', name: 'profile_index')]
+    // #[Route('/{slug}', name: 'profile_index')]
     public function index(User $user = null): Response
     {
-      if(!$user){
-        dd('erreur404');
-      }
-
       return $this->render('profile/index.html.twig', [
         'user' => $user,
         'capsules' => $user->getCapsules()
